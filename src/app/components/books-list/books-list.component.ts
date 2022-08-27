59760a486108f0faf905ca43b39a2c8ebdab7c8b
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-08-27 03:10:38 +0300

    display list of books from db

--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CrudService } from './../../service/crud.service';
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
   styleUrls: ['./books-list.component.css']
 })
 export class BooksListComponent implements OnInit {
-
-  constructor() { }
-
+  
+  Books:any = [];
+  constructor(private crudService: CrudService) { }
   ngOnInit(): void {
+    this.crudService.GetBooks().subscribe(res => {
+      console.log(res)
+      this.Books =res;
+    });    
   }
-
+  delete(id:any, i:any) {
+    console.log(id);
+    if(window.confirm('Do you want to go ahead?')) {
+      this.crudService.deleteBook(id).subscribe((res) => {
+        this.Books.splice(i, 1);
+      })
+    }
+  }
 }
